07a581e14061b58d598265dd5560ad538b5d363e
ArtSyv <artalsiv@gmail.com>
2022-02-22 17:45:35 +0200

    just heroku

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+// const comperssion = require('comperssion');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist/project_lab'));
+// app.use(comperssion());
+
+app.use(express.static(path.join(__dirname + '/dist/project_lab')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/project_lab/index.html'));
 });
 
-app.listen(process.env.PORT || 8080); 
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`server started ${port}`));
